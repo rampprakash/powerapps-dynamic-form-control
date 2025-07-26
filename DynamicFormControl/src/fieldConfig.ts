508bda864fa56f6bba1f8814bdf357a16777a4ef
rampprakash <88942457+rampprakash@users.noreply.github.com>
2025-07-26 14:53:14 -0600

    Update fieldConfig.ts

--- DynamicFormControl/src/fieldConfig.ts
+++ DynamicFormControl/src/fieldConfig.ts
@@ -2,43 +2,31 @@ export const fields = [
   {
     name: "firstname",
     type: "string",
-    displayname: "First Name",
-    isSearchEnabled: true,
-    isResultNeeded: true
+    displayname: "First Name"
   },
   {
     name: "lastname",
     type: "string",
-    displayname: "Last Name",
-    isSearchEnabled: true,
-    isResultNeeded: true
+    displayname: "Last Name"
   },
   {
     name: "birthdate",
     type: "date",
-    displayname: "Date of Birth",
-    isSearchEnabled: true,
-    isResultNeeded: true
+    displayname: "Date of Birth"
   },
   {
     name: "emailaddress1",
     type: "email",
-    displayname: "Email",
-    isSearchEnabled: true,
-    isResultNeeded: true
+    displayname: "Email"
   },
   {
     name: "mobilephone",
     type: "phone",
-    displayname: "Phone",
-    isSearchEnabled: true,
-    isResultNeeded: true
+    displayname: "Phone"
   },
   {
     name: "address1_composite",
     type: "string",
-    displayname: "Address",
-    isSearchEnabled: false,
-    isResultNeeded: true
+    displayname: "Address"
   }
 ];
